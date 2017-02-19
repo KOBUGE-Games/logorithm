f6c9a1b4e2799f492e0224e3fa54296353d36490
Rémi Verschelde <rverschelde@gmail.com>
2017-02-19 12:42:49 +0100

    Add glow animation when word is valid

--- game/game.gd
+++ game/game.gd
@@ -6,6 +6,7 @@ onready var language_pack = LanguagePackManager.get_language_pack("en")
 
 var selection_letters = []
 var selection_string = ""
+var valid_word = false
 
 ## Callbacks ##
 
@@ -20,16 +21,27 @@ func on_letter_selected(letter):
 			selection_letters.pop_back()
 			selection_string = selection_string.substr(0, selection_string.length() - 1)
 			letter.set_highlight(false)
+			if valid_word:
+				letter.get_node("animation").stop()
 		else: # discard whole selection
 			for letter in selection_letters:
 				letter.set_highlight(false)
+				if valid_word:
+					letter.get_node("animation").stop()
 			selection_letters = []
 			selection_string = ""
 	elif selection_string.empty() or letter.adjoins(selection_letters.back()):
 		selection_letters.append(letter)
 		selection_string += letter.get_character()
 		letter.set_highlight(true)
-	print(selection_string)
 
 	if language_pack.has_word(selection_string):
 		print("%s is a valid word." % selection_string)
+		valid_word = true
+		for letter in selection_letters:
+			letter.get_node("animation").play("glow")
+	elif valid_word:
+		valid_word = false
+		for letter in selection_letters:
+			letter.get_node("animation").stop()
+			letter.set_highlight(true)
